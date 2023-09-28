716bc322a684ec4f722221b8d53145592d04f74e
justinas.indrasius <justinas.indrasius@birkle-it.com>
2023-09-28 08:45:47 +0300

    Initialized spring app

---
+++ src/test/java/com/example/spring_framework_6/SpringFramework6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_framework_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramework6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
